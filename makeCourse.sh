a397a879785dd7e46e22e634d8173a6af4fbd4b6
Alejandro-S23 <alejandrosouza277353@gmail.com>
2025-08-03 19:56:07 -0300

    Criado o cabeçalo de clean code

--- makeCourse.sh
+++ makeCourse.sh
@@ -1,4 +1,34 @@
-#/!bin/bash
+#!/usr/bin/env bash
+#
+# makeCourse.sh - Gerenciamento de Pastas para Cursos
+#
+# Site:       https://www.linkedin.com/in/alejandro-s-santos
+# Autor:      Alejandro Souza
+# Manutenção: Alejandro Souza
+#
+# ------------------------------------------------------------------------ #
+#  Este programa irá auxiliar os usuários na organização de diretórios dos 
+#  seus cursos, aumentando sua produtividade e foco nos estudos.
+#
+#  Exemplos:
+#      $ ./makeCourse.sh.sh "Shell Script"
+#      Neste exemplo o script criará um diretório com o nome Shell Script,
+#      juntamente com as subpastas: "PDFs", "Praticas" e "Resumos";
+# ------------------------------------------------------------------------ #
+# Histórico:
+#
+#   v1.0 26/07/2025, Alejandro:
+#      - Criada a função de criação do dirétio *curso* com as subpastas.
+#       
+# ------------------------------------------------------------------------ #
+# Testado em:
+#   bash 4.4.19
+# ------------------------------------------------------------------------ #
+# ------------------------------- VARIÁVEIS ----------------------------------------- #
+
+# ------------------------------------------------------------------------ #
+
+# ------------------------------- EXECUÇÃO ----------------------------------------- #
 
 if [ -z "$1" ]; then
 	echo "Use: $0 \"Nome Curso\""
@@ -13,3 +43,5 @@ echo "Criando os diretórios para o curso $curso na pasta $inst"
 mkdir -p "$curso"/{PDFs,Resumos,Pratica}
 
 echo "Diretório de $curso criada com sucesso!"
+
+# ------------------------------------------------------------------------ #
