a302112787bb8623f06f967ec7c055b4e8ea0258
Alejanro S <alejandrosouza277353@gmail.com>
2025-08-08 15:07:46 -0300

    Adicionado parâmetro -d de debug, e aplicado algumas boas práticas

--- makeCourse.sh
+++ makeCourse.sh
@@ -28,41 +28,48 @@
 #      - Adicionado Cabeçalho Sólido no código;
 #      - Adicionado o parâmetro "-h";
 #      - Adicionada a possibilidade de inserir um instituto como diretório raiz;
+#
+#   v2.1 07/08/2025, Alejandro:
+#      - Adicionado parâmetro de debug
+#      - Pequenas mudanças de código sem alteração nas funções
 #       
 # ------------------------------------------------------------------------ #
 # Testado em:
 #   bash 4.4.19
 # ------------------------------------------------------------------------ #
 # ------------------------------- VARIÁVEIS ----------------------------------------- #
-VERSION="v2.0"
+VERSION="v2.1"
 HELP_MSG="
   $(basename $0) - [OPÇÕES]
-    prompt base $VERSION - $0 'Nome do Curso
 
+    -d - Debug da criação das pastas (Se utilizado, deve ser o primeiro parâmetro)
     -h - Menu de Ajuda
+    -v - Versão do programa
     -i - Insere uma pasta de Instituto como diretório raiz (Deve ser acompanhado do nome do instituto)
 "
 COURSE_NAME=""
 INST_NAME=""
-
+DEBUGGIN=0
 # ------------------------------------------------------------------------ #
 # ------------------------------- EXECUÇÃO ----------------------------------------- #
 
-while [[ -n "$1" ]]
+while test -n "$1"
 do
 # É um comando?
   if [[ "$1" = -* ]]; then
     case "$1" in
-      -h) echo "$HELP_MSG" && exit 0                        ;;
-      -i) shift && INST_NAME="$1/"                          ;;
+      -h) echo "$HELP_MSG" && exit 0                            ;;
+      -i) [ $2 ] && INST_NAME="$2/" && shift                    ;;
+      -v) echo "$(basename $0) $VERSION" && exit 0              ;;
+      -d) DEBUGGIN=1                                            ;;
       *) echo "Opção Inválida, Digite -h para ajuda." && exit 1 ;;
     esac
   else
     COURSE_NAME="$1"
   fi
+
   shift
 done
-
+[[ $DEBUGGIN -eq 1 ]] && set -x
 mkdir -p "$INST_NAME$COURSE_NAME"/{PDFs,Resumos,Pratica}
-
 # ------------------------------------------------------------------------ #
